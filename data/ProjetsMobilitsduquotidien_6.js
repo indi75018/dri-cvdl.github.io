8540d2b356afaace6303b4a2e51ac38706102781
indi75018 <77441597+indi75018@users.noreply.github.com>
2021-03-03 18:19:58 +0100

    Add files via upload

---
+++ data/ProjetsMobilitsduquotidien_6.js
@@ -0,0 +1,11 @@
+var json_ProjetsMobilitsduquotidien_6 = {
+"type": "FeatureCollection",
+"name": "ProjetsMobilitsduquotidien_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Thématiqu": "Mobilité du quotidien", "Subvention": "44 000 €", "Coût": "55 000 €", "Projet": "Installation d'un garage à vélos sécurisé" }, "geometry": { "type": "Point", "coordinates": [ 2.513972, 46.734525 ] } },
+{ "type": "Feature", "properties": { "Thématiqu": "Mobilité du quotidien", "Subvention": "44 000 €", "Coût": "55 000 €", "Projet": "Installation d'un garage à vélos sécurisé" }, "geometry": { "type": "Point", "coordinates": [ 0.685263, 47.43449 ] } },
+{ "type": "Feature", "properties": { "Thématiqu": "Mobilité du quotidien", "Subvention": "4 880 000 €", "Coût": "6 100 000 €", "Projet": "Rénovation mi- vie de 3 rames électriques ZTER circulant sur l'axe Tours-Orléans. La maîtrise d'ouvrage est assurée par SNCF Mobilité" }, "geometry": null },
+{ "type": "Feature", "properties": { "Thématiqu": "Mobilité du quotidien", "Subvention": "480 000 €", "Coût": "600 000 €", "Projet": "10 gares aménagées. La maîtrise d'ouvrage est assurée par Gares et Connexions" }, "geometry": null }
+]
+}
